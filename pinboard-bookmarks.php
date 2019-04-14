4c83e40f0bd80a48d98d56fe20875634e89f721a
Ross Wintle <ross@oikos.org.uk>
2019-04-14 20:20:40 +0100

    Plugin scaffolding

---
+++ pinboard-bookmarks.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Pinboard Bookmarks
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Fetch bookmarks from Pinboard
+ * Author:          Ross Wintle
+ * Author URI:      https://rosswintle.uk
+ * Text Domain:     pinboard-bookmarks
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Pinboard_Bookmarks
+ */
+
+// Your code starts here.
