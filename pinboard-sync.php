86f48d51f64f0b6fe146e31285dca542449f7a60
Ross Wintle <ross@oikos.org.uk>
2019-04-22 12:08:25 +0100

    Remove plugin URI - a duplicate of the author URI is not allowed

--- pinboard-sync.php
+++ pinboard-sync.php
@@ -1,7 +1,6 @@
 <?php
 /**
  * Plugin Name:     Pinboard Sync
- * Plugin URI:      https://rosswintle.uk
  * Description:     Fetch bookmarks from Pinboard into a custom post type and custom taxonomy
  * Author:          Ross Wintle
  * Author URI:      https://rosswintle.uk
