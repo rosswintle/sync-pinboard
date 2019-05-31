4e9cb9b4bafa5da557b27fce489fb61ef0c95d9e
Ross Wintle <ross@oikos.org.uk>
2019-05-31 08:49:16 +0100

    Right. This darned block NEARLY works now!

--- blocks/pins/block.js
+++ blocks/pins/block.js
@@ -4,6 +4,43 @@ const Fragment = wp.element.Fragment;
 const InspectorControls = wp.editor.InspectorControls;
 const TimePicker = wp.components.TimePicker;
 
+function createSyncPinboardList(props) {
+	const pins = props.attributes.pinJson;
+	const listItems = [];
+
+	pins.forEach( function(item, index) {
+		listItems.push(
+			el(
+				'li',
+				null,
+				el(
+					'a',
+					//{ href: ''},
+					null,
+					item.title.rendered),
+				el(
+					'span',
+					null,
+					' - ' + item.content.rendered)
+				));
+	});
+
+	const list = el(
+		'ul',
+		null,
+		...listItems);
+
+	return list;
+}
+
+async function fetchPins (startDate, endDate, props) {
+	// TODO: localise this
+	let response = await fetch(`/wp-json/wp/v2/pinboard-bookmark/?after=${startDate}&before=${endDate}&per_page=100`);
+	let apiJson = await response.json();
+	console.log(apiJson);
+	props.setAttributes({ pinJson: apiJson });
+}
+
 
 const blockStyle = {
     backgroundColor: '#900',
@@ -16,18 +53,23 @@ registerBlockType( 'sync-pinboard/pins', {
     icon: 'post-status',
     category: 'common',
     attributes: {
-    	startDate: {
+		startDate: {
     		type: 'string',
     		default: moment().format('YYYY-MM-DDTHH:mm:ss')
     	},
     	endDate: {
     		type: 'string',
     		default: moment().format('YYYY-MM-DDTHH:mm:ss')
+    	},
+    	pinJson: {
+    		type: 'array',
+    		default: [],
     	}
     },
     edit: function( props ) {
     	let startDate = props.attributes.startDate;
     	let endDate = props.attributes.endDate;
+
     	return [
     	el(
     		Fragment,
@@ -42,32 +84,32 @@ registerBlockType( 'sync-pinboard/pins', {
     					currentTime: startDate,
     					onChange: function (timestamp) {
     						props.setAttributes({ startDate: timestamp });
+    						fetchPins(props.attributes.startDate, props.attributes.endDate, props);
     					}
     				}
-    			),
+    				),
     			el(
     				TimePicker,
     				{
     					label: 'End date/time',
     					currentTime: endDate,
-						onChange: function (timestamp) {
-							props.setAttributes({ endDate: timestamp });
-						}
-					}
+    					onChange: function (timestamp) {
+    						props.setAttributes({ endDate: timestamp });
+    						fetchPins(props.attributes.startDate, props.attributes.endDate, props);
+    					}
+    				}
+    				)
     			)
-    		)
-    	),
-    	el(
-    		'p',
-    		{ style: blockStyle },
-    		'Showing pins from between ' + props.attributes.startDate + ' and ' + props.attributes.endDate
-    		)
+    		),
+    	createSyncPinboardList(props)
     	];
     },
     save: function( props ) {
+    	const list = createSyncPinboardList(props);
     	return el(
-    		'p',
+    		'div',
     		{ style: blockStyle },
-    		'Showing (front) pins from between ' + props.attributes.startDate + ' and ' + props.attributes.endDate);
-    }
+	    	list
+	    );
+    },
 } );
